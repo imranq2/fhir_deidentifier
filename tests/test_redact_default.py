8427ec2629d2f5439e0c0909dadb3ff018f51e46
Imran Qureshi <imranq2@hotmail.com>
2024-09-24 11:18:51 -0700

    default to redact

--- tests/test_redact_default.py
+++ tests/test_redact_default.py
@@ -13,6 +13,8 @@ def test_health_check(rest_client: TestClient):
 def test_anonymize_redact_default(rest_client):
     print("")
     """Test the /anonymize endpoint with valid input"""
+
+    # https://github.com/microsoft/Tools-for-Health-Data-Anonymization/blob/master/docs/FHIR-anonymization.md#fhir-path-rules
     config = {
         "fhirVersion": "R4",
         "processingError": "raise",
@@ -23,10 +25,8 @@ def test_anonymize_redact_default(rest_client):
             {"path": "Resource.id", "method": "cryptoHash"},
             {"path": "nodesByType('Reference').reference", "method": "cryptoHash"},
             {"path": "Group.name", "method": "redact"},
-            {
-                "path": "Patient.birthDate",
-                "method": "dateshift"
-            },
+            {"path": "Patient.birthDate", "method": "dateshift"},
+            {"path": "Patient.gender", "method": "keep"},
             {"path": "Resource", "method": "redact"},  # by default remove all fields
         ],
         "parameters": {
@@ -54,19 +54,22 @@ def test_anonymize_redact_default(rest_client):
         "resource": resource
     })
 
-    assert response.status_code == 200
-    assert response.json() == {'birthDate': '1975-01-25',
-                               'id': '0d9b28fe8cc6aa8ab33f70305576fca5431b0d9339ac83d17e4d4ca882fae668',
-                               'meta': {'security': [{'code': 'REDACTED',
-                                                      'display': 'redacted',
-                                                      'system': 'http://terminology.hl7.org/CodeSystem/v3-ObservationValue'},
-                                                     {'code': 'CRYTOHASH',
-                                                      'display': 'cryptographic hash function',
-                                                      'system': 'http://terminology.hl7.org/CodeSystem/v3-ObservationValue'},
-                                                     {'code': 'PERTURBED',
-                                                      'display': 'exact value is replaced with another exact '
-                                                                 'value'}]},
-                               'resourceType': 'Patient'}
+    assert response.status_code == 200, response.text
+    assert response.json() == {
+        'birthDate': '1975-01-25',
+        'gender': 'male',
+        'id': '0d9b28fe8cc6aa8ab33f70305576fca5431b0d9339ac83d17e4d4ca882fae668',
+        'meta': {'security': [{'code': 'REDACTED',
+                               'display': 'redacted',
+                               'system': 'http://terminology.hl7.org/CodeSystem/v3-ObservationValue'},
+                              {'code': 'CRYTOHASH',
+                               'display': 'cryptographic hash function',
+                               'system': 'http://terminology.hl7.org/CodeSystem/v3-ObservationValue'},
+                              {'code': 'PERTURBED',
+                               'display': 'exact value is replaced with another exact '
+                                          'value'}]},
+        'resourceType': 'Patient'
+    }
 
 
 def test_anonymize_invalid_input(rest_client):
