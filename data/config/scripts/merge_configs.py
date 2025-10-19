55494832b1c3acb31f62bb4493da2712bf63c514
Imran Qureshi <imranq2@hotmail.com>
2025-10-19 12:14:02 -0700

    add Organization

--- data/config/scripts/merge_configs.py
+++ data/config/scripts/merge_configs.py
@@ -33,16 +33,19 @@ placeholder_replacements: Dict[str, str] = {
     "SUBSTITUTE_PERSON_NAME": "Jane Doe",
     "TRUSTED_CODE_SYSTEMS": trusted_code_system_regex,
     "PRACTITIONER_NAME_1": "Doogie Howser, MD",
-    "PRACTITIONER_NAME_2": "Meredith Grey, MD",
-    "PRACTITIONER_NAME_3": "John Dorian, MD",
-    "PRACTITIONER_NAME_4": "Lisa Cuddy, MD",
-    "PRACTITIONER_NAME_5": "Gregory House, MD",
+    "PRACTITIONER_NAME_2": "Gregory House, MD",
+    "PRACTITIONER_NAME_3": "Meredith Grey, MD",
+    "PRACTITIONER_NAME_4": "Christopher Turk, MD",
+    "PRACTITIONER_NAME_5": "John Dorian, MD",
     "PRACTITIONER_UUID_1": "e985b181-ed44-42bf-94d0-f09a4fe72d10",
     "PRACTITIONER_UUID_2": "078e6f48-495d-4e80-843e-af3cd64b37d8",
     "PRACTITIONER_UUID_3": "5dcf1d7d-e880-44f3-8fa0-39edf02439a8",
     "PRACTITIONER_UUID_4": "2548693e-f44e-44b6-a905-002dd124bbb5",
     "PRACTITIONER_UUID_5": "e3112d92-ced0-42ad-bccf-ede4721adcf4",
-
+    "ORGANIZATION_NAME_1": "South Medical Center",
+    "ORGANIZATION_NAME_2": "City Medical Center",
+    "ORGANIZATION_UUID_1": "2500b32e-88e1-4e65-af2b-0bddca822f09",
+    "ORGANIZATION_UUID_2": "36cf9f91-4022-4d20-9dad-7ab0ebb3fe0e"
 }
 
 
